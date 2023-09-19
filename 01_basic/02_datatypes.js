25f445b09ab89f0c5417bcfc8dae1d4ea03fc49e
Dksng07 <dksng070901@gmail.com>
2023-09-19 15:49:32 +0530

    config

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strcit"; // treat all js code as a newer version 
+
+// alert (3+3) // we are using node js not a browser 
+
+console.log(3+
+    3 ) // code readbility should be high  
+
+
+console.log("dheeraj")
+
+let name ="dheeraj"
+let age = 18
+let state ;
+
+//number => 2 to power 53
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+// at the end of console.log semicolon is not necessary...
